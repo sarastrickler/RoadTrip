d053b519dcf750a330909c4669edff7e64fba9a7
Joe Ricioppo <spuufmail@mac.com>
2012-11-07 14:02:53 -0800

    Initial Commit

---
+++ RoadTrip/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RoadTrip
+//
+//  Created by Sara Pendleton Strickler on 11/7/12.
+//  Copyright (c) 2012 Sara Pendleton Strickler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RTAppDelegate class]));
+    }
+}
